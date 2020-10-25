251f7bf27bcfc0463ac15a80cc1465daf7e2b1a3
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-10-25 22:44:33 +0200

    creates up block and down block, fixed generator new numbers

--- src/App.js
+++ src/App.js
@@ -21,23 +21,27 @@ class App extends React.Component {
     let x = Math.floor(Math.random() * 4)
     let y = Math.floor(Math.random() * 4)
     let z = [...this.state.arrays]
-    if (z.every(c => c.every(c => c !== 0))){
-     alert('Game Over')
-     this.refreshArray([[0,0,0,0],
-                        [0,0,0,0],
-                        [0,0,0,0],
-                        [0,0,0,0],])
-    }
+    // if (z.every(c => c.every(c => c !== 0))){
+    //  alert('Game Over')
+    //  this.refreshArray([[0,0,0,0],
+    //                     [0,0,0,0],
+    //                     [0,0,0,0],
+    //                     [0,0,0,0],])
+    // }
 
     if (z[x][y] === 0){
-      z[x][y] = Math.random() > 0.5 ? 4 : 2
+      z[x][y] = 2
       this.refreshArray(z)
     } else this.generateNumbers()
   }
   
   componentDidMount() {
     document.addEventListener('keypress', this.swipeLeft)
+    document.addEventListener('keypress', this.swipeRight)
+    document.addEventListener('keypress', this.swipeUp)
+    document.addEventListener('keypress', this.swipeDown)
   }
+  
 
   //Game Panel
 
@@ -47,7 +51,6 @@ class App extends React.Component {
 
     if (event.code === 'KeyA'){
       this.left()
-      this.generateNumbers()
       this.generateNumbers()
     }
   }
@@ -81,10 +84,162 @@ class App extends React.Component {
 
   //Swipe Right Components
 
-  //Swipe Down Components
+  swipeRight = (event) => {
+
+    if (event.code === 'KeyD'){
+      this.right()
+      this.generateNumbers()
+    }
+  }
+
+  right() {
+    let stateArray = [...this.state.arrays];
+    let exitArray = [];
+
+    for (let i = 0; i < stateArray.length; i++){
+     let filterElements = stateArray[i].filter(elem => elem !== 0);
+     let additionDoneElements = [];
+     for (let m = 0; m < filterElements.length; m++){
+        if (filterElements[m] === filterElements[m + 1]){
+          additionDoneElements.push(filterElements[m] * 2)
+          m++
+        } else {
+          additionDoneElements.push(filterElements[m])
+        }
+     }
+     let emptyElements = new Array(4 - additionDoneElements.length).fill(0);
+
+     for (let n = 0; n < additionDoneElements.length; n++){
+      emptyElements.push(additionDoneElements[n])
+     }
+     exitArray.push(emptyElements)
+ 
+    }
+
+    this.refreshArray(exitArray)
+  }
 
   //Swipe Up Components
 
+  swipeUp = (event) => {
+
+    if (event.code === 'KeyW'){
+      this.up()
+      this.generateNumbers()
+    }
+  }
+
+  up() {
+    let stateArray = [...this.state.arrays]
+    let x = []
+    let one = [];
+    let two = [];
+    let tree = [];
+    let four = [];
+
+    for (let i = 0; i < stateArray.length; i++) {
+      one.push(stateArray[i][0])
+      two.push(stateArray[i][1])
+      tree.push(stateArray[i][2])
+      four.push(stateArray[i][3])
+    }
+
+    
+
+    let y = [one, two, tree, four]
+    let res = []
+     for (let j = 0; j < y.length; j++){
+       let filters = y[j].filter(a => a !== 0)
+       let filtered = []
+       for (let h = 0; h < filters.length; h++){
+         if (filters[h] === filters[h+1]){
+           filtered.push(filters[h] * 2)
+           h++
+         } else filtered.push(filters[h])
+       }
+       let z = new Array(4 - filtered.length).fill(0)
+  
+       for (let y = 0; y < z.length; y++){
+        filtered.push(z[y])
+       }
+       
+       res.push(filtered)
+     }
+
+     for (let i = 0; i < res.length; i++) {
+       let r = []
+      for (let h = 0; h < res.length; h++) {
+        r.push(res[h][i])
+      }
+      x.push(r)
+     }
+    
+     this.refreshArray(x)
+     
+
+    
+  }
+
+  //Swipe Down Components
+
+  swipeDown = (event) => {
+
+    if (event.code === 'KeyS'){
+      this.down()
+      this.generateNumbers()
+    }
+  }
+
+  down() {
+    let stateArray = [...this.state.arrays]
+    let x = []
+    let one = [];
+    let two = [];
+    let tree = [];
+    let four = [];
+
+    for (let i = 0; i < stateArray.length; i++) {
+      one.push(stateArray[i][0])
+      two.push(stateArray[i][1])
+      tree.push(stateArray[i][2])
+      four.push(stateArray[i][3])
+    }
+
+    
+
+    let y = [one, two, tree, four]
+    let res = []
+     for (let j = 0; j < y.length; j++){
+       let filters = y[j].filter(a => a !== 0)
+       let filtered = []
+       for (let h = 0; h < filters.length; h++){
+         if (filters[h] === filters[h+1]){
+           filtered.push(filters[h] * 2)
+           h++
+         } else filtered.push(filters[h])
+       }
+       let z = new Array(4 - filtered.length).fill(0)
+  
+       for (let y = 0; y < filtered.length; y++){
+        z.push(filtered[y])
+       }
+       
+       res.push(z)
+     }
+
+     for (let i = 0; i < res.length; i++) {
+       let r = []
+      for (let h = 0; h < res.length; h++) {
+        r.push(res[h][i])
+      }
+      x.push(r)
+     }
+    
+     this.refreshArray(x)
+     
+
+    
+  }
   
   
   
